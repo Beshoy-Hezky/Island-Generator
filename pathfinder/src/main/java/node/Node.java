2e252791ab8ecda0cfcc5bfcbcdfdeb96472dc71
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-03-29 14:55:07 -0400

    F01: Add node coordinates

--- pathfinder/src/main/java/node/Node.java
+++ pathfinder/src/main/java/node/Node.java
@@ -3,9 +3,9 @@ package node;
 public class Node<T> {
     T node;
 
-    double x; // x, y, z
-    double y;
-    double z;
+    private double x; // x, y, z
+    private double y;
+    private double z;
 
     /**
      *
@@ -13,8 +13,24 @@ public class Node<T> {
      */
     public Node(T node){
         this.node = node;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
     }
 
+    /**
+     *
+     * @param node the object
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param z z coordinate
+     */
+    public Node(T node, float x, float y, float z){
+        this.node = node;
+        this.x = x;
+        this.x = x;
+        this.x = x;
+    }
     /**
      *
      * @return the node
