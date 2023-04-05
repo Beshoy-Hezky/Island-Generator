1f2988c89dc2811480e701687686737c24725140
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-04-05 13:59:07 -0400

    F02: implement Dijksta's algorithm

--- pathfinder/src/main/java/shortestpath/DijkstraShortestPath.java
+++ pathfinder/src/main/java/shortestpath/DijkstraShortestPath.java
@@ -1,20 +1,14 @@
-/*
 package shortestpath;
-
 import edge.Edge;
-import edge.Edges;
 import graph.IGraph;
-import graph.UndirectedGraphs;
 import node.Node;
-
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class DijkstraShortestPath<T> implements ShortestPath<T>{
     @Override
     public Map <Node<T>, Node<T>> shortestpath(IGraph<T> graph, Node<T> node1, Node<T> node2) {
-        Map<Node, Node> path = new HashMap<>();
-        Map<Node, Double> cost = new HashMap<>();
+        Map<Node<T>, Node<T>> path = new HashMap<>();
+        Map<Node<T>, Double> cost = new HashMap<>();
         graph.getNodes().forEach(node->{
             path.put(node,null);
             cost.put(node, Double.MAX_VALUE);
@@ -22,23 +16,20 @@ public class DijkstraShortestPath<T> implements ShortestPath<T>{
 
         path.put(node1 , node1);
         cost.put(node1, 0d);
-        PriorityQueue<Map.Entry<Node,Double>> queue = new PriorityQueue<>(Comparator.comparingDouble(Map.Entry::getValue));
+        PriorityQueue<Map.Entry<Node<T>, Double>> queue = new PriorityQueue<>(Comparator.comparingDouble(Map.Entry::getValue));
         queue.add(Map.entry(node1, 0d));
 
         while(!queue.isEmpty()){
-            Node<T> m = queue.remove().getKey();
-            for()
+            Node<T> current = queue.remove().getKey();
+            for(Edge<T> edge : graph.EdgesOf(current)){
+                if(cost.get(current) + edge.getWeight() < cost.get(edge.getNode2())){     // check later if edge.getNode2 is correct
+                    path.put(edge.getNode2() , current);
+                    cost.put(edge.getNode2() , cost.get(current) + edge.getWeight());
+                    queue.add(Map.entry(edge.getNode2(), cost.get(edge.getNode2())));
+
+                }
+            }
         }
-
-
-
-
-        return null;
+        return path;
     }
-
-
-
-
 }
-
-*/
