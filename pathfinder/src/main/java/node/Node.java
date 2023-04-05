d7dafdc2d6511205b86d66e3968a9d7fb4d30a89
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-04-05 13:56:29 -0400

    F01: fix Node class technical debt

--- pathfinder/src/main/java/node/Node.java
+++ pathfinder/src/main/java/node/Node.java
@@ -3,31 +3,12 @@ package node;
 public class Node<T> {
     T node;
 
-    private double x; // x, y, z
-    private double y;
-    private double z;
 
     /**
      * @param node pass in a node
      */
     public Node(T node) {
         this.node = node;
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
-    }
-
-    /**
-     * @param node the object
-     * @param x    x coordinate
-     * @param y    y coordinate
-     * @param z    z coordinate
-     */
-    public Node(T node, float x, float y, float z) {
-        this.node = node;
-        this.x = x;
-        this.x = x;
-        this.x = x;
     }
 
     /**
@@ -36,18 +17,5 @@ public class Node<T> {
     public T getNode() {
         return this.node;
     }
-
-    public double getX() {
-        return this.x;
-    }
-
-    public double getY() {
-        return this.y;
-    }
-
-    public double getZ() {
-        return this.z;
-    }
-
 }
 
